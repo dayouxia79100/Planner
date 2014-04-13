7430415ec864d69c9120b032097ebcfe0fec8793
Summer Cui <summerc2028@gmail.com>
2014-04-13 15:26:42 -0400

    item class added

---
+++ src/com/example/plannermockup/model/Item.java
@@ -0,0 +1,61 @@
+package com.example.plannermockup.model;
+
+import java.io.Serializable;
+
+import android.R.integer;
+
+public class Item implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1968646824181506979L;
+	
+	private int iid;
+	private String itemName;
+	private int quantity;
+	private User user;
+	
+	public Item() {
+		
+	}
+	
+	public Item(int _iid, String _itemName, int _quantity, User _user) {
+		iid = _iid;
+		itemName = _itemName;
+		quantity = _quantity;
+		user = _user;
+	}
+	
+	public void setIid(int _iid) {
+		iid = _iid;
+	}
+	
+	public int getIid() {
+		return iid;
+	}
+	
+	public void setItemName(String _itemName) {
+		itemName = _itemName;
+	}
+	
+	public String getItemName() {
+		return itemName;
+	}
+	
+	public void setQuantity(int _quantity) {
+		quantity = _quantity;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setUser(User _user) {
+		user = _user;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+}
